a6371d0ecfeaa6115b9b0d662fb0463b3f45c80b
WillSplaine <91668586+WillSplaine@users.noreply.github.com>
2023-11-03 16:15:15 +0000

    Map Elements - Platform Routes
    added routes to floor 1 - floor 4 (probuilder)

--- My project/Assets/Scripts/PlayerCam.cs
+++ My project/Assets/Scripts/PlayerCam.cs
@@ -27,7 +27,7 @@ public class PlayerCam : MonoBehaviour
         yRotation += mouseX;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 75f);
 
         // rotate cam and orientation
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
