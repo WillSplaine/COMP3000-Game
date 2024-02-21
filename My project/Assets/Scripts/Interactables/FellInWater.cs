55ee9923b1ef8da6b9b56f091d55ab1574a7b777
WillSplaine <91668586+WillSplaine@users.noreply.github.com>
2024-02-21 02:49:04 +0000

    Death UI + Organization of scripts

---
+++ My project/Assets/Scripts/Interactables/FellInWater.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FellInWater : MonoBehaviour
+{
+
+    public GameObject DeathScreen;
+    public static bool playerFell;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        playerFell = true;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        DeathScreen.SetActive(true);
+        Time.timeScale = 0f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
